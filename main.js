edeafaea5fc6fbcbba8ddea302b8c532eb5fd759
KaliLinuxIII <128271072+KaliLinuxIII@users.noreply.github.com>
2024-04-08 12:25:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ let display = document.getElementById('display');
 function checkPassword() {
     if (submitBox.value.toLowerCase() == 'fortnite') {
         display.innerHTML = 'Vlad is NOT good at fortnite';
+    } else if (submitbox.value.toLowerCase() == 'battlepass') {
+        display.innerHTML = 'You like men';
     } else {
-        display.innerHTML = 'Wrong password';
-    };
+        display.innerHTML = 'Wrong password!';
 };
 
 submitButton.addEventListener('click', checkPassword);
